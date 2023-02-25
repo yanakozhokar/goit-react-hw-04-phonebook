a11dc846c6488ab7b6e237b0e6f50251e0fb4ff5
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-02-25 14:40:49 +0200

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,21 +7,18 @@ import ContactList from './ContactList/ContactList';
 
 const App = () => {
   const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem('contacts')) ?? '';
-  });
-  const [filter, setFilter] = useState('');
-  const isFirstRender = useRef(true);
-
-  useEffect(() => {
-    if (!contacts && isFirstRender) {
-      setContacts([
+    return (
+      JSON.parse(localStorage.getItem('contacts')) ?? [
         { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
         { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
         { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
         { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]);
-      return;
-    }
+      ]
+    );
+  });
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
