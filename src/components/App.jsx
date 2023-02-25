64266145ac14d13644b1373c289e5a1f3f00abf5
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-02-25 16:54:56 +0200

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import Container from './Container/Container';
 import ContactForm from './ContactForm/ContactForm';
